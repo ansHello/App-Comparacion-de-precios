6df3edb8464f3797e9fc981a6133b97ef6e2b28a
Anthony Dell <e1750574137@uisrael.edu.ec>
2026-05-27 18:54:24 -0500

    Final

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,7 +13,7 @@ namespace ComparacionPrecios
         protected override Window CreateWindow(IActivationState? activationState)
         {
             // 1. Envolvemos la vista de Login en una NavigationPage para permitir flujo secuencial
-            var paginaNavegacion = new NavigationPage(new VHome());
+            var paginaNavegacion = new NavigationPage(new VLogin());
 
             // 2. Retornamos una nueva ventana que contiene nuestra página de navegación
             return new Window(paginaNavegacion);
